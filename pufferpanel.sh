9b59712dd33c121614e738d074e502d7ccd2ea0b
jshruwyd <159998785+jshruwyd@users.noreply.github.com>
2024-09-15 14:03:31 +0700

    Update pufferpanel.sh

--- pufferpanel.sh
+++ pufferpanel.sh
@@ -1,9 +1,12 @@
 # A Script to auto install Pufferpanel 
 
+sudo apt install sudo systemctl -y
+curl -o /bin/systemctl https://raw.githubusercontent.com/gdraheim/docker-systemctl-replacement/master/files/docker/systemctl3.py
 curl -s https://packagecloud.io/install/repositories/pufferpanel/pufferpanel/script.deb.sh | sudo bash
 sudo apt-get install pufferpanel
 sudo systemctl enable pufferpanel
-sudo pufferpanel user add --email foxytoux@gmail.com --name foxytoux --password Fox21200 --admin
+sudo pufferpanel user add
 sudo systemctl enable --now pufferpanel
+./ngrok.sh
 
 # I uses https://docs.pufferpanel.com/en/2.x/installing.html to make this easy script, please look over there is there is any problem
